94c1ff780d5da9a51e14f5bbd37af633b82a9001
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2020-10-26 16:59:21 +0100

    Statistics backend

---
+++ ExpressionEngine/Maths/StatisticResult.cs
@@ -0,0 +1,101 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Text;
+
+namespace ExpressionEngine.Maths
+{
+    public sealed class StatisticResult
+    {
+        /// <summary>
+        /// Minimum value
+        /// </summary>
+        public double Minimum { get; internal set; }
+
+        /// <summary>
+        /// Maximum value
+        /// </summary>
+        public double Maximum { get; internal set; }
+
+        /// <summary>
+        /// Range is the length of the smallest interval which contains all the data.
+        /// </summary>
+        public double Range { get; internal set; }
+
+        /// <summary>
+        /// Median is the number separating the higher half of a sample, a population, or a probability distribution, from the lower half.
+        /// </summary>
+        public double Median { get; internal set; }
+
+        /// <summary>
+        /// Sum of all valuess
+        /// </summary>
+        public double Sum { get; internal set; }
+
+        /// <summary>
+        /// Variance is the measure of the amount of variation of all the scores for a variable (not just the extremes which give the range).
+        /// </summary>
+        public double Variance { get; internal set; }
+
+        /// <summary>
+        /// The Standard Deviation of a statistical population, a data set, or a probability distribution is the square root of its variance.
+        /// </summary>
+        public double StandardDeviation { get; internal set; }
+
+        /// <summary>
+        /// Mode is the value that occurs the most frequently in a data set or a probability distribution.
+        /// </summary>
+        public double Mode { get; internal set; }
+
+        /// <summary>
+        /// Root Mean Square is the measure of the magnitude of a varying series. 
+        /// </summary>
+        public double RootMeanSquare { get; internal set; }
+
+        public double Average { get; set; }
+
+        public StatisticResult()
+        {
+            Minimum = double.NaN;
+            Maximum = double.NaN;
+            Median = double.NaN;
+            Range = double.NaN;
+            Mode = double.NaN;
+            Sum = double.NaN;
+            Variance = double.NaN;
+            StandardDeviation = double.NaN;
+            RootMeanSquare = double.NaN;
+            Average = double.NaN;
+        }
+
+        public string ToString(IFormatProvider format)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}: {1}", nameof(Minimum), Minimum.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(Maximum), Maximum.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(Range), Range.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(Median), Median.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(Mode), Mode.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(Sum), Sum.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(Average), Average.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(RootMeanSquare), RootMeanSquare.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(Variance), Variance.ToString(format));
+            sb.AppendFormat("{0}: {1}", nameof(StandardDeviation), StandardDeviation.ToString(format));
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToString(CultureInfo.InvariantCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExpressionEngine/Maths/Statistics.cs
@@ -0,0 +1,90 @@
+﻿//-----------------------------------------------------------------------------
+// (c) 2020 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpressionEngine.Maths
+{
+    public static class Statistics
+    {
+        public static StatisticResult Statistic(params double[] items)
+        {
+            return Statistic(items);
+        }
+
+        public static StatisticResult Statistic(IEnumerable<double> items)
+        {
+            var ordered = items.OrderBy(x => x).ToArray();
+
+            StatisticResult result = new StatisticResult();
+
+            if (ordered.Length > 0)
+            {
+                result.Maximum = ordered[ordered.Length - 1];
+                result.Minimum = ordered[0];
+            }
+
+            double sum = 0;
+            double mean = 0;
+            double M2 = 0;
+            double currentValue = 0;
+            double mode = 0;
+            int occurence = 0;
+            int maxOccurence = 0;
+            double meanSquare = 0;
+            foreach (var item in items)
+            {
+                sum += item;
+                double delta = item - mean;
+                mean += delta / ordered.Length;
+                M2 += delta * (item - mean);
+                meanSquare += Math.Pow(item, 2);
+
+                Mode(ref currentValue, ref mode, ref occurence, ref maxOccurence, item);
+
+            }
+
+            int itemIndex = ordered.Length / 2;
+
+            if (itemIndex % 2 == 0)
+                result.Median = (ordered[itemIndex] + ordered[itemIndex - 1]) / 2.0;
+            else
+                result.Median = ordered[itemIndex];
+
+            if (maxOccurence > 1)
+                result.Mode = mode;
+
+            result.Sum = sum;
+            result.Variance = M2 / (ordered.Length - 1);
+            result.StandardDeviation = Math.Sqrt(result.Variance);
+            result.Range = result.Maximum - result.Minimum;
+            result.Average = sum / ordered.Length;
+            result.RootMeanSquare = Math.Sqrt(meanSquare / ordered.Length);
+
+            return result;
+        }
+
+        private static void Mode(ref double current, ref double mode, ref int count, ref int max, double item)
+        {
+            if (current != item)
+            {
+                current = item;
+                count = 1;
+            }
+            else
+            {
+                ++count;
+            }
+
+            if (count > max)
+            {
+                max = count;
+                mode = current;
+            }
+        }
+    }
+}
