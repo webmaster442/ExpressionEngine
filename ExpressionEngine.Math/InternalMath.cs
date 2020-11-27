fa8dff8275e12b7618de01177f54324a3b2c852a
webmaster442 <ruzsinszki.gabor@gmail.com>
2020-11-27 20:40:59 +0100

    Big Float basics

---
+++ ExpressionEngine.Math/InternalMath.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+namespace ExpressionEngine.BigNumber
+{
+    internal static class InternalMath
+    {
+		public static BigInteger Gcd(BigInteger a, BigInteger b)
+		{
+            a = BigInteger.Abs(a);
+			b = BigInteger.Abs(b);
+			BigInteger result;
+			if (a == BigInteger.One || b == BigInteger.One)
+			{
+				result = 1L;
+			}
+			else
+			{
+				while (true)
+				{
+					BigInteger num = a % b;
+					if (num == BigInteger.Zero)
+					{
+						break;
+					}
+					a = b;
+					b = num;
+				}
+				result = b;
+			}
+			return result;
+		}
+
+		public static BigInteger Lcm(BigInteger a, BigInteger b)
+		{
+			return (a * b) / Gcd(a, b);
+		}
+	}
+}
---
+++ ExpressionEngine.Tests/Maths/BigFloatTests.cs
@@ -0,0 +1,19 @@
+﻿using ExpressionEngine.BigNumber;
+using NUnit.Framework;
+
+namespace ExpressionEngine.Tests.Maths
+{
+    [TestFixture]
+    public class BigFloatTests
+    {
+        [TestCase(1, 2, "0.5")]
+        [TestCase(2, 4, "0.5")]
+        [TestCase(1, 3, "0.3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333")]
+        public void TestToString(long a, long b, string expected)
+        {
+            var result = new BigFloat(a, b).ToString();
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
