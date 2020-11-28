aadbbae8f83d8e6a647a7b2cfb23497d331e9e72
webmaster442 <ruzsinszki.gabor@gmail.com>
2020-11-28 19:07:01 +0100

    Work on Big maths

---
+++ ExpressionEngine.Math/BigMath.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace ExpressionEngine.BigNumber
+{
+    public static class BigMath
+    {
+        public static readonly BigFloat Pi = BigFloat.Parse("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679", CultureInfo.InvariantCulture);
+        public static readonly BigFloat E = BigFloat.Parse("2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274", CultureInfo.InvariantCulture);
+
+        public static BigFloat Abs(BigFloat value)
+        {
+            BigInteger num = BigInteger.Abs(value.Numerator);
+            return new BigFloat(num, value.Denominator);
+        }
+        public static BigFloat Ceiling(BigFloat value)
+        {
+            BigInteger numerator = value.Numerator;
+            BigInteger denominator = value.Denominator;
+            if (numerator < 0)
+                numerator -= BigInteger.Remainder(numerator, denominator);
+            else
+                numerator += denominator - BigInteger.Remainder(numerator, denominator);
+
+            return new BigFloat(numerator, denominator);
+        }
+
+        public static BigFloat Floor(BigFloat value)
+        {
+            return Ceiling(value) - BigFloat.One;
+        }
+
+        public static BigFloat Truncate(BigFloat value)
+        {
+            BigInteger numerator = value.Numerator;
+            numerator -= BigInteger.Remainder(numerator, value.Denominator);
+            return new BigFloat(numerator, value.Denominator);
+        }
+
+        public static BigFloat Log10(BigFloat value)
+        {
+            var v = BigInteger.Log10(value.Numerator) - BigInteger.Log10(value.Denominator);
+            return (BigFloat)v;
+        }
+
+        public static BigFloat Log(BigFloat value, double baseValue)
+        {
+            var v = BigInteger.Log(value.Numerator, baseValue) - BigInteger.Log(value.Denominator, baseValue);
+            return (BigFloat)v;
+        }
+
+        public static BigFloat Root(BigFloat value, int n)
+        {
+            if (n == 1)
+                return value;
+
+            BigFloat x = value;
+            BigFloat root = BigFloat.Zero;
+
+            BigFloat desiredroot = new BigFloat(1, n);
+            for (int i=0; i<15; i++)
+            {
+                root = desiredroot * (x + (value / x));
+                x = root;
+            }
+
+            return root;
+        }
+    }
+}
---
+++ ExpressionEngine.Tests/Maths/BigMathTests.cs
@@ -0,0 +1,65 @@
+﻿using ExpressionEngine.BigNumber;
+using NUnit.Framework;
+using System;
+using System.Globalization;
+
+namespace ExpressionEngine.Tests.Maths
+{
+    [TestFixture]
+    public class BigMathTests
+    {
+        private void CallMethodUnderTest(string input, string expected, Func<BigFloat, BigFloat> method)
+        {
+            var number = BigFloat.Parse(input, CultureInfo.InvariantCulture);
+            var op = method.Invoke(number).ToString();
+            Assert.AreEqual(expected, op);
+        }
+
+        [TestCase("-0.5", "0.5")]
+        [TestCase("0.5", "0.5")]
+        public void TestAbs(string input, string expected)
+        {
+            CallMethodUnderTest(input, expected, BigMath.Abs);
+        }
+
+        [TestCase("7.03", "7")]
+        [TestCase("7.64", "7")]
+        [TestCase("0.12", "0")]
+        [TestCase("-0.12", "-1")]
+        [TestCase("-7.1", "-8")]
+        [TestCase("-7.6", "-8")]
+        public void TestFloor(string input, string expected)
+        {
+            CallMethodUnderTest(input, expected, BigMath.Floor);
+        }
+
+        [TestCase("7.03", "8")]
+        [TestCase("7.64", "8")]
+        [TestCase("0.12", "1")]
+        [TestCase("-0.12", "0")]
+        [TestCase("-7.1", "-7")]
+        [TestCase("-7.6", "-7")]
+        public void TestCeiling(string input, string expected)
+        {
+            CallMethodUnderTest(input, expected, BigMath.Ceiling);
+        }
+
+        [TestCase("32.7865", "32")]
+        [TestCase("-32.9012", "-32")]
+        public void TestTruncate(string input, string expected)
+        {
+            CallMethodUnderTest(input, expected, BigMath.Truncate);
+        }
+
+        [TestCase("2", 2, "1.4142135623730950488016887242096980785696718753769480731766797379907324784621070388503875343276415727")]
+        [TestCase("3", 2, "1.7320508075688772935274463415058723669428052538103806280558069794519330169088000370811461867572485756")]
+        [TestCase("2", 1, "2")]
+        [TestCase("1", 2, "1")]
+        public void TestRoot(string input, int root, string expected)
+        {
+            var number = BigFloat.Parse(input, CultureInfo.InvariantCulture);
+            var op = BigMath.Root(number, root).ToString();
+            Assert.AreEqual(expected, op);
+        }
+    }
+}
